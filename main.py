a96aefa6aa104a80dcdd263fb07caf436a81aa35
putuagra <iputuagra@gmail.com>
2024-10-01 14:10:20 +0700

    feat: refactor name file

--- main.py
+++ main.py
@@ -1,99 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import win32com.client as win32
-from PIL import Image
-import os
 
-def convert_csv(csv_name, excel_name):
-    df_csv = pd.read_csv(csv_name, sep="|", header=0)
-    percentage_df = df_csv.copy()
-    for col_num in range(1, len(df_csv.columns) - 1): 
-        percentage_df[df_csv.columns[col_num]] = (df_csv[df_csv.columns[col_num]] / df_csv["total_requests"]) * 100
-    with pd.ExcelWriter(excel_name, engine="xlsxwriter") as writer:
-        percentage_df.to_excel(writer, index=False, sheet_name="Sheet1", startrow=0)
-        workbook = writer.book
-        worksheet_chart = workbook.add_worksheet("Sheet2")
-        # worksheet = writer.sheets["Sheet1"]
-        # df_csv = percentage_df.copy()
-        chart = workbook.add_chart({"type": "column", "subtype": "stacked"})
-        
-        # Configure the series of the chart from the DataFrame data
-        for col_num in range(1, len(percentage_df.columns) - 1):
-            color = ""
-            if percentage_df.columns[col_num] in ["2s-3s", "3s-4s"]:
-                color = "#32CD32"
-            elif percentage_df.columns[col_num] == "4s-5s":
-                color = "#FF4500"
-            elif percentage_df.columns[col_num] in ["5s-6s", "6s-7s", "7s-8s"]:
-                color = "#FFD700"
-            elif percentage_df.columns[col_num] in ["8s-9s", "9s-10s", ">10s"]:
-                color = "red"
-            
-            chart.add_series(
-                {
-                    "name": ["Sheet1", 0, col_num],
-                    # 'categories': ['Sheet1', 1, 0, len(df)-1, 0],
-                    # 'values':     ['Sheet1', 1, col_num, len(df)-1, col_num],
-                    "categories": ["Sheet1", 1, 0, 20, 0],
-                    "values": ["Sheet1", 1, col_num, 20, col_num],
-                    "gap": 5,
-                    "fill": {"color": color},
-                }
-            )
+csv_file = 'output.csv'
+df = pd.read_csv(csv_file, delimiter='|')
 
-        # Configure the chart axes
-        chart.set_y_axis(
-            {
-                "major_gridlines": {"visible": False},
-                "format": "#.0%",
-                'min': 0, 
-                'max': 100,
-            }
-        )
+def generateResponseTime(the_data, typeUri):
+    # Define response time intervals (bins) for categorizing the response times
+    bins = [2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, np.inf]  # np.inf for values greater than 10 seconds
+    labels = ['2s-3s', '3s-4s', '4s-5s', '5s-6s', '6s-7s', '7s-8s', '8s-9s', '9s-10s', '>10s']
 
-        chart.set_size({"width": 1000, "height": 400})
-
-        # Insert the chart into the worksheet
-        # worksheet.insert_chart("M2", chart)
-        worksheet_chart.insert_chart("A1", chart)
-        
-def convert_pdf():
-    excel = win32.Dispatch("Excel.Application")
-    excel.Visible = False
-
-    workbook = excel.Workbooks.Open(r"E:\Metrodata\BNI\Backend-resptime\backend.xlsx")
+    if typeUri == 'backend':
+        # Create a new column for binned response times
+        the_data['response_time_interval'] = pd.cut(the_data['backend_exec_time'], bins=bins, labels=labels, right=False)
+        grouped = the_data.groupby(['backend_uri', 'response_time_interval']).size().unstack(fill_value=0)
+        grouped['total_requests'] = grouped[['2s-3s', '3s-4s', '4s-5s', '5s-6s', '6s-7s', '7s-8s', '8s-9s', '9s-10s', '>10s']].sum(axis=1)
+        grouped = grouped.reset_index()
+        df_sorted = grouped.sort_values(by='total_requests', ascending=False)
+        # df_sorted.to_csv('backend_resp_time.csv', sep='|')
+        # print(df_sorted)
+        return df_sorted
+    elif typeUri == 'surrounding':
+        # Create a new column for binned response times
+        the_data['response_time_interval'] = pd.cut(the_data['surr_exec_time'], bins=bins, labels=labels, right=False)
+        grouped = the_data.groupby(['surr_uri', 'response_time_interval']).size().unstack(fill_value=0)
+        grouped['total_requests'] = grouped[['2s-3s', '3s-4s', '4s-5s', '5s-6s', '6s-7s', '7s-8s', '8s-9s', '9s-10s', '>10s']].sum(axis=1)
+        grouped = grouped.reset_index()
+        df_sorted = grouped.sort_values(by='total_requests', ascending=False)
+        # df_sorted.to_csv('backend_resp_time.csv', sep='|')
+        # print(df_sorted)
+        return df_sorted
     
-    # Activate Sheet2
-    sheet2 = workbook.Sheets("Sheet2")
+def generateChart(df,typeUri,pdfName):
+    df = df.nlargest(20, 'total_requests')
+
+    # Pilih kolom untuk stacked columns (semua kolom kecuali '*_uri' dan 'total_requests')
+    columns_to_plot = df.columns[1:-1]
+
+    # Set type_uri sebagai index
+    if typeUri == 'Backend':
+        df.set_index('backend_uri', inplace=True)
+    elif typeUri == 'Surrounding':
+        df.set_index('surr_uri', inplace=True)
+
+    # Hitung persentase dari setiap kolom (dari total_requests)
+    df_percent = df[columns_to_plot].div(df['total_requests'], axis=0) * 100
+
+    # Plot stacked bar chart dengan sumbu horizontal sebagai type_uri dan sumbu vertikal sebagai persentase
+    df_percent.plot(kind='bar', stacked=True, figsize=(10, 7))
+
+    # Menambahkan label dan judul
+    plt.ylabel('Persentase Requests (%)')
+    plt.xlabel(f'{typeUri} URI')
+    plt.title(f'Top 20 Total Request {typeUri}')
+    plt.legend(title='Rentang Waktu', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    plt.savefig(pdfName, format='pdf')
+    # Tampilkan diagram
+    # plt.show()
     
-    chart_object = sheet2.ChartObjects(1)  # Assuming the first chart on the sheet
-    chart_image_path = r"E:\Metrodata\BNI\Backend-resptime\chart_image.png"
-    chart_object.Chart.Export(chart_image_path, "PNG")
-    
-    # Close the workbook
-    workbook.Close(False)
-    
-    # Step 3: Rotate the image using PIL
-    img = Image.open(chart_image_path)
-    img_rotated = img.rotate(0, expand=True)  # Rotate 90 degrees to the right
-    rotated_image_path = r"E:\Metrodata\BNI\Backend-resptime\rotated_chart_image.png"
-    img_rotated.save(rotated_image_path)
-
-    # Step 4: Save the rotated image as a PDF
-    pdf_path = r"E:\Metrodata\BNI\Backend-resptime\chart.pdf"
-    img_rotated.save(pdf_path, "PDF", resolution=100.0)
-
-    # Clean up: Remove the temporary image file if needed
-    if os.path.exists(chart_image_path):
-        os.remove(chart_image_path)
-    
-    # Quit Excel
-    excel.Quit()
-
-
-def main():
-    convert_csv("backend_resp_time_v2.csv", "backend.xlsx")
-    convert_pdf()
-
 if __name__ == "__main__":
     print("Running.....")
-    main()
+    df_backend = generateResponseTime(df, "backend")
+    df_surr = generateResponseTime(df, "surrounding")
+    # print(df_backend)
+    generateChart(df_backend,'Backend','top_20_total_requests_backend.pdf')
+    generateChart(df_surr,'Surrounding','top_20_total_requests_surrounding.pdf')
+    # print(df_surr)
